d1ef8bf9a7b7bfde4a8f117e0709d65c4e25f036
thzthix <95135403+thzthix@users.noreply.github.com>
2023-03-27 04:52:32 +0900

    implemented "%" button feature

--- index.js
+++ index.js
@@ -38,32 +38,34 @@ const buttonValues = {
   nine: "9",
   zero: "0",
 };
-function calculateExpression() {
-  num1 = Number(currentState.calculateNum1);
-  num2 = Number(currentState.calculateNum2);
+function calculateExpression(num1, num2, symbol) {
+  num1 = Number(num1);
+  num2 = Number(num2);
   console.log(`num1:${num1},num2:${num2}`);
+  let result;
 
-  switch (currentState.currentSymbol) {
+  switch (symbol) {
     case "+":
-      currentState.calculateNum1 = (num1 + num2).toString();
-
+      result = (num1 + num2).toString();
       break;
     case "-":
-      currentState.calculateNum1 = (num1 - num2).toString();
+      result = (num1 - num2).toString();
       break;
+
     case "*":
-      currentState.calculateNum1 = (num1 * num2).toString();
+      result = (num1 * num2).toString();
       break;
 
     case "/":
-      currentState.calculateNum1 = (num1 / num2).toString();
+      result = (num1 / num2).toString();
       break;
 
     default:
       console.log(`Sorry, error occurred during calculation`);
   }
-  currentState.calculateNum2 = "";
+
   displayResult();
+  return result;
 }
 function displayNum(num, modify) {
   if (!modify) {
@@ -76,57 +78,84 @@ function storeNumbers() {
   if (currentState.calculateNum1 === "0") {
     currentState.calculateNum1 = displayNumericInput.textContent;
   } else {
-    console.log("kldjlskadjs");
     currentState.calculateNum2 = displayNumericInput.textContent;
   }
 }
 function handleSymbolClicked(event) {
-  storeNumbers();
-  currentState.numericAfterSymbolClicked = false;
+  currentState.symbolClicked = true;
+  const clickedSymbol = buttonValues[`${event.target.id}`];
+  if (!currentState.numericAfterSymbolClicked) {
+    if (currentState.calculateNum1 === "0") {
+      currentState.calculateNum1 = displayNumericInput.textContent;
+    }
+    currentState.currentSymbol = clickedSymbol;
+    return;
+  }
+
   console.log(
     ` calculateNum1: ${currentState.calculateNum1} symbol:${
       buttonValues[event.target.id]
     } calculateNum2:${currentState.calculateNum2}`
   );
 
-  const clickedSymbol = buttonValues[`${event.target.id}`];
-  if (!currentState.previousSymbol) {
-    currentState.previousSymbol = clickedSymbol;
-    return;
-  } else {
-    currentState.currentSymbol = currentState.previousSymbol;
-    console.log("i clickeddd");
-    calculateExpression();
-    currentState.previousSymbol = clickedSymbol;
+  if (currentState.calculateNum2) {
+    currentState.calculateNum1 = calculateExpression(
+      currentState.calculateNum1,
+      currentState.calculateNum2,
+      currentState.previousSymbol
+    );
+    currentState.currentSymbol = clickedSymbol;
+    currentState.previousSymbol = "";
   }
+  if (currentState.numericAfterSymbolClicked) {
+    currentState.calculateNum2 = displayNumericInput.textContent;
+    currentState.previousSymbol = currentState.currentSymbol;
+    console.log("i clickeddd");
+    currentState.calculateNum1 = calculateExpression(
+      currentState.calculateNum1,
+      currentState.calculateNum2,
+      currentState.previousSymbol
+    );
+    currentState.currentSymbol = clickedSymbol;
+    currentState.previousSymbol = "";
+  }
+  currentState.numericAfterSymbolClicked = false;
+  currentState.calculateNum2 = "";
 }
 
 function handleNumericClicked(event) {
-  // symbolClicked = false;
-  // if (calculateNum1 && symbolClicked && !numericAfterSymbolClicked) {
-  //   displayNumericInput.textContent = "";
-  //   displayNumericInput.textContent = buttonValues[event.target.id];
-  //   numericAfterSymbolClicked = true;
-  //   return;
-  // }
-  // numericAfterSymbolClicked = true;
-  // displayNum(buttonValues[event.target.id], false);
   const clickedNumber = buttonValues[`${event.target.id}`];
-  if (!currentState.numericAfterSymbolClicked) {
+  if (currentState.symbolClicked) {
     currentState.numericAfterSymbolClicked = true;
     displayNumericInput.textContent = "";
     displayNumericInput.textContent = buttonValues[event.target.id];
+    currentState.symbolClicked = false;
     return;
   }
   displayNum(clickedNumber, false);
+  currentState.symbolClicked = false;
 }
 
 function handleModifyNumricClicked(event) {
   if (event.target.id === "percent") {
-    displayNum(
-      (Number(displayNumericInput.textContent) / 100).toString(),
-      true
+    const percentage = Number(currentState.calculateNum1) / 100;
+    const num2 = displayNumericInput.textContent;
+    console.log(`percentage:${percentage}`);
+    currentState.previousSymbol = currentState.currentSymbol;
+    currentState.currentSymbol = "*";
+    const result = calculateExpression(
+      percentage,
+      num2,
+      currentState.currentSymbol
     );
+
+    currentState.calculateNum2 = result;
+    console.log(`result:${result}`);
+    displayNum(currentState.calculateNum2, true);
+
+    currentState.currentSymbol = currentState.previousSymbol;
+    currentState.numericAfterSymbolClicked = true;
+
     storeNumbers();
   } else if (event.target.id === "enter-sign") {
     displayNum((Number(displayNumericInput.textContent) * -1).toString(), true);
@@ -138,14 +167,13 @@ function displayResult() {
   displayNumericInput.textContent = currentState.calculateNum1.toString();
 }
 function onClickEqual() {
-  // if (!symbolClicked) {
-  //   calculateNum1 = displayNumericInput.textContent;
-  //   displayResult();
-  //   console.log(`clicked equl:${calculateNum1}`);
-  //   // calculateNum1 = 0;
-  //   return;
-  // }
-  calculateExpression();
+  currentState.calculateNum2 = displayNumericInput.textContent;
+  if (currentState.numericAfterSymbolClicked) {
+    currentState.calculateNum1 = calculateExpression(
+      currentState.currentSymbol
+    );
+    numericAfterSymbolClicked = false;
+  }
 }
 equalButton.addEventListener("click", onClickEqual);
 function resetDisplayPanel(event) {
